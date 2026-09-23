1ff4fddd6836db80f195ec7471f67c1bee9db1f3
Kevin Miller <kemiller2002@gmail.com>
2026-09-23 17:46:18 +0200

    feat(folio): register remaining core print primitives (#11)

--- src/components/register.js
+++ src/components/register.js
@@ -3,10 +3,20 @@ const elementNames = [
   "ef-print-title-page",
   "ef-print-section",
   "ef-print-back-page",
+  "ef-print-header",
+  "ef-print-footer",
+  "ef-print-page-number",
   "ef-print-columns",
   "ef-print-sidebar",
+  "ef-print-layer",
   "ef-print-break",
   "ef-print-keep",
+  "ef-print-callout",
+  "ef-print-figure",
+  "ef-print-table",
+  "ef-print-code",
+  "ef-print-toc",
+  "ef-print-note",
 ];
 
 class EchelonPrintElement extends HTMLElement {}
